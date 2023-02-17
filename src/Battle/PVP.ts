127d026539d49a504ce41c42a115dbef5acd3872
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-02-17 11:44:57 -0300

    Requisito 10: Classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,25 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private player1: Fighter;
+  private player2: Fighter;
+
+  constructor(p1: Fighter, p2: Fighter) {
+    super(p1);
+    this.player1 = p1;
+    this.player2 = p2;
+  }
+
+  private turns() {
+    while (this.player1.lifePoints !== -1 && this.player2.lifePoints !== -1) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+  }
+
+  fight(): number {
+    this.turns();
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
